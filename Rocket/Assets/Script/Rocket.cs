c4911ce807f6d48819bb53b61671f2988a197d83
B533P-02\533 <jeepsboucher@gmail.com>
2018-08-24 16:59:40 -0400

    Création de la fusée

---
+++ Rocket/Assets/Script/Rocket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    public Rigidbody rocketRigidBody;
+    [SerializeField] public float Jump = 150;
+    [SerializeField] public float Horizontal = 3;
+    // Use this for initialization
+    void Start () {
+        rocketRigidBody = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Jump") > 0)
+        {
+            rocketRigidBody.AddForce(new Vector3(0, Jump, 0));
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            rocketRigidBody.AddForceAtPosition(new Vector3(Horizontal, 0, 0), new Vector3(2, 0, 0));
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            rocketRigidBody.AddForceAtPosition(new Vector3(-Horizontal, 0, 0), new Vector3(2, 0, 0));
+        }
+    }
+}
